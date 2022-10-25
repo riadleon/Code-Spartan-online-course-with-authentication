1f0ea046b2c8fa5dd5371f9e5acbe91aa1d30a4a
Riad Shalahin Leon <105214634+riadleon@users.noreply.github.com>
2022-10-25 22:04:12 +0600

    foooter and sidebar

--- src/pages/Courses/Courses.js
+++ src/pages/Courses/Courses.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link, useLoaderData } from 'react-router-dom';
 import CourseCard from '../Shared/CourseCard/CourseCard';
+import logo from '../../assests/logo/logo.png'
 
 const Courses = () => {
     const allCourses = useLoaderData();
@@ -9,14 +10,12 @@ const Courses = () => {
         <div>
             <div className="hero min-h-screen bg-base-200">
                 <div className="hero-content flex-col lg:flex-row">
-                    <div className="h-full p-3 space-y-2 w-60 dark:bg-gray-900 dark:text-gray-100">
+                    <div className="h-full p-3 space-y-6 w-60 dark:bg-gray-900 dark:text-gray-100">
                         <div className="flex items-center p-2 space-x-4">
-                            {/* <img src="https://source.unsplash.com/100x100/?portrait" alt="" className="w-12 h-12 rounded-full dark:bg-gray-500" /> */}
+                            <img className="w-12 h-12 rounded-full dark:bg-gray-500" src={logo} alt="" />
                             <div>
-                                {/* <h2 className="text-lg font-semibold">Leroy Jenkins</h2>
-                                <span className="flex items-center space-x-1">
-                                    <a rel="noopener noreferrer" href="#" className="text-xs hover:underline dark:text-gray-400">View profile</a>
-                                </span> */}
+                                <h2 className="text-lg font-semibold">Code Spartans</h2>
+
                             </div>
                         </div>
                         <div className="divide-y divide-gray-700">
@@ -35,8 +34,8 @@ const Courses = () => {
                     <div className='grid grid-cols-3 gap-2'>
                         {
                             allCourses.map(course => <CourseCard
-                            key={course.id}
-                            course={course}
+                                key={course.id}
+                                course={course}
                             ></CourseCard>)
                         }
                     </div>
