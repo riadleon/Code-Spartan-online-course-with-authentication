7f5b6d4757663b905fc1ee84e198a09a3970ab9c
Riad Shalahin Leon <105214634+riadleon@users.noreply.github.com>
2022-10-26 15:37:51 +0600

    environment variable updated

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDS5bH0hxlW6p4Esm_Tf0i8Hmsg03Ujxn4",
-  authDomain: "code-spartans-client.firebaseapp.com",
-  projectId: "code-spartans-client",
-  storageBucket: "code-spartans-client.appspot.com",
-  messagingSenderId: "798169583734",
-  appId: "1:798169583734:web:2c6ac097dca5f82c9fcf57"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId
 };
 
 // Initialize Firebase
